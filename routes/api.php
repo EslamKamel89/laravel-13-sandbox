cb9e58bbdf59a8b2864a5399a5672a5444e564c2
EslamKamel89 <elsamkamelforex@gmail.com>
2026-04-08 00:28:00 +0200

    feat(api): add posts resource with factory, migrations, and basic auth login route

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::prefix('auth')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+});
+Route::resource('/posts', PostController::class);
